fef6a5f775f6575733fab9a2ae09b28f34181f77
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2025-12-11 08:14:11 +0200

    Add vim library to luals config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -153,6 +153,13 @@ vim.lsp.config("lua_ls", {
 				'selene.yml',
 				'.git',
 		},
+		settings = {
+				Lua = {
+						workspace = {
+								library = vim.api.nvim_get_runtime_file("", true)
+						}
+				}
+		}
 })
 vim.lsp.enable("lua_ls")
 
