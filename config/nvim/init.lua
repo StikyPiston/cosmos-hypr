6da1606c060dc0d7118485fb6558472ae0d2116e
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2025-12-11 08:26:59 +0200

    Remove the vim library from luals config because it's too slow

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -154,13 +154,6 @@ vim.lsp.config("lua_ls", {
 				'selene.yml',
 				'.git',
 		},
-		settings = {
-				Lua = {
-						workspace = {
-								library = vim.api.nvim_get_runtime_file("", true)
-						}
-				}
-		}
 })
 vim.lsp.enable("lua_ls")
 
