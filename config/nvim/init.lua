f12c5df41afe4b9cccb4603cc8b4f6420f1a27b3
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2025-12-13 14:31:24 +0200

    Fix the indentation that I forgor to fix

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -86,15 +86,15 @@ require("nvim-treesitter.configs").setup({
 	auto_install = true,
 
 	highlight = {
-			enable = true,
+		enable = true,
 	},
 
 	incremental_selection = {
-			enable = true,
+		enable = true,
 	},
 
 	indent = {
-			enable = true,
+		enable = true,
 	},
 })
 
